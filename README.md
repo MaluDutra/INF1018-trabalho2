47af71edcb89795d1cb51c22bc024d126935b2d5
MaluDutra <mldutraga@gmail.com>
2024-11-19 11:39:55 -0300

    Commit inicial

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # INF1018-trabalho2
+Trabalho para a disciplina de Software Básico.
---
+++ cria_func.c
@@ -0,0 +1,8 @@
+/* Gustavo Rocha Barros 2310505 3WA */
+/* Maria Luiza Dutra Gonzalez de Almeida 2310509 3WA */
+#include <stdio.h>
+#include "cria_func.h"
+
+void cria_func (void* f, DescParam params[], int n, unsigned char codigo[]){
+    
+}
