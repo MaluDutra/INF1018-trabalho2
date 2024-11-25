54b3400cf0c9f744a9809aeeff9bc91f64d5a049
MaluDutra <mldutraga@gmail.com>
2024-11-24 21:25:19 -0300

    Início: passando dos parâmetros da função para registradores temporários (inteiro: param e fix | ponteiro: param)

--- cria_func.c
+++ cria_func.c
@@ -4,6 +4,22 @@
 #include "cria_func.h"
 
 void cria_func(void* f, DescParam params[], int n, unsigned char codigo[]){
+    union{ //para facilitar na hora de pegar e passar os valores dos inteiros
+        int i;
+        char c[4]; //int -- 4 bytes
+    } inteiro;
+
+    union{ //para facilitar na hora de pegar e passar os valores dos ponteiros
+        void* p;
+        char c[8]; //ponteiro -- 8 bytes
+    } ponteiro;
+
+    unsigned char reg_temp[3][3]={{0xf8, 0xf9, 0xfa}, //mover %xdi para %r8, %r9 e %r10
+                                      {0xf0, 0xf1, 0xf2},  //mover %xsi para %r8, %r9 e %r10
+                                      {0xd0, 0xd1, 0xd2}}; //mover %xdx para %r8, %r9 e %r10
+
+    unsigned char reg_fix_l[3] = {0xb8, 0xb9, 0xba}; //mover $x para %r8d, %r9d e %r10d
+    //início
     int pos = 0;
 
     //registro de ativação
@@ -12,23 +28,53 @@ void cria_func(void* f, DescParam params[], int n, unsigned char codigo[]){
     codigo[pos++] = 0x89;
     codigo[pos++] = 0xe5;
 
-    for (int i = 0; i < n; i++){ //preparando os parâmetros para a chamada
+    //salvando os registradores
+    //xdi --> r8
+    //xsi --> r9
+    //xdx --> r10
+    int aux_param = 0;
+    for (int i = 0; i < n; i++, aux_param++){ 
         if (params[i].tipo_val == INT_PAR){ //parâmetro é um inteiro
             if (params[i].orig_val == PARAM){
-                //é um parâmetro não amarrado
+                //é um parâmetro não amarrado -- 4 bytes
+                codigo[pos++] = 0x41;
+                codigo[pos++] = 0x89;
+                codigo[pos++] = reg_temp[aux_param][i];
+
             } else if (params[i].orig_val == FIX){
-                //é um parâmetro fixo
-            } else{
-                //é um endereço
+                //é um parâmetro fixo -- 4 bytes
+                codigo[pos++] = 0x41;
+                codigo[pos++] = reg_fix_l[i];
+
+                inteiro.i = params[i].valor.v_int;
+                for (int aux = 0; aux < 4; aux++){
+                    codigo[pos++] = inteiro.c[aux];
+                }
+
+
+            } else if (params[i].orig_val == IND){
+                //é um endereço que deve ser guardado -- 8 bytes
+                //nsei de jeito nenhum ainda
+
             }
 
         } else{ //parâmetro é um ponteiro
             if (params[i].orig_val == PARAM){
-                //é um parâmetro não amarrado
+                //é um parâmetro não amarrado -- 8 bytes
+                codigo[pos++] = 0x49;
+                codigo[pos++] = 0x89;
+                codigo[pos++] = reg_temp[aux_param][i];
+
             } else if (params[i].orig_val == FIX){
-                //é um parâmetro fixo
-            } else{
-                //é um endereço
+                //é um parâmetro fixo -- 8 bytes
+                //nsei de jeito nenhum ainda
+
+
+            } else if (params[i].orig_val == IND){
+                //é um endereço -- 8 bytes
+                //nsei de jeito nenhum ainda
+
+
             }
         }
     }
@@ -36,6 +82,7 @@ void cria_func(void* f, DescParam params[], int n, unsigned char codigo[]){
     //call *%rax
     codigo[pos++] = 0xff;
     codigo[pos++] = 0xd0;
+
     codigo[pos++] = 0xc9; //leave
     codigo[pos++] = 0xc3; //ret
 }
--- foo.s
+++ foo.s
@@ -3,7 +3,38 @@
 foo:
    pushq %rbp
    movq %rsp, %rbp
-  
+   
+   movl %edi, %r8d
+   movl %esi, %r8d
+   movl %edx, %r8d
+   movl $0x1234, %r8d
+
+   movq %rdi, %r8
+   movq %rsi, %r8
+   movq %rdx, %r8
+   movq $0, %r8
+   
+   movl %edi, %r9d
+   movl %esi, %r9d
+   movl %edx, %r9d
+   movl $0, %r9d
+
+   movq %rdi, %r9
+   movq %rsi, %r9
+   movq %rdx, %r9
+   movq $0, %r9
+
+   movl %edi, %r10d
+   movl %esi, %r10d
+   movl %edx, %r10d
+   movl $0, %r10d
+
+   movq %rdi, %r10
+   movq (%rdi), %r10
+   movq %rsi, %r10
+   movq %rdx, %r10
+   movq $0xFFFFFFFFFFFFFFFF, %r10 /*não entendi*/
+
    movq %rdi, %rax
    call *%rax
    
